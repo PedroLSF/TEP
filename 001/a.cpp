33f3dba9180395841c91372ebd7cbfcfba331152
pedrolucas0 <siqueirafernandes02@gmail.com>
2023-05-27 10:23:09 -0300

    First

---
+++ 001/a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace::std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> skewers(2 * N);
+    for (int i = 0; i < 2 * N; i++) {
+        cin >> skewers[i];
+    }
+
+    sort(skewers.begin(), skewers.end());
+
+    int maxIngredients = 0;
+    for (int i = 0; i < 2 * N; i+=2) {
+        int espetox = skewers[i];
+        int espetoy = skewers[i+1];
+        maxIngredients += (espetox < espetoy) ? espetox : espetoy;
+
+        // int totalIngredients = skewers[i] + skewers[2 * N - i - 1];
+        // maxIngredients = min(maxIngredients, totalIngredients);
+    }
+
+    cout << maxIngredients << endl;
+
+    return 0;
+}
